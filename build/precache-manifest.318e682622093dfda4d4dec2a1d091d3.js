64b2b4616e7e79fb7d5dca6f07f2a9b67984d3d7
Ankit Mishra <1507256+mishra-ankit@users.noreply.github.com>
2020-08-13 11:36:33 +0200

    build

---
+++ build/precache-manifest.318e682622093dfda4d4dec2a1d091d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbf00540b53e73fe54aad56543a47d78",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53d705d174884ff5ecea",
+    "url": "/static/css/main.2094bdcb.chunk.css"
+  },
+  {
+    "revision": "04d663a0e45399dda57b",
+    "url": "/static/js/2.079e839d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.079e839d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53d705d174884ff5ecea",
+    "url": "/static/js/main.35cc74bf.chunk.js"
+  },
+  {
+    "revision": "e1f4633d0b250b75e305",
+    "url": "/static/js/runtime-main.2cb59220.js"
+  }
+]);
